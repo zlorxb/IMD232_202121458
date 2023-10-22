dce08010ddce487fad787e456e848d3e93a725d9
zlorxb <143993169+zlorxb@users.noreply.github.com.>
2023-10-23 05:01:59 +0900

    w6

---
+++ src/w6/assignment/step1/script/Particle.js
@@ -0,0 +1,46 @@
+class Particle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    // this.vel.rotate((TAU / 360) * random(-150, -30));
+    this.vel = createVector(0, 3);
+    this.acc = createVector(0, 0);
+
+    this.rad = 12;
+    this.lifeSpan = 255;
+
+    //파티클 랜덤 색
+    this.color = color(random(255), random(255), random(255));
+
+    this.rotationA = random(360);
+    this.rotationS = random(-0.1, 0.1);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+    this.lifeSpan -= 2;
+
+    this.rotationA += this.rotationS;
+  }
+
+  display() {
+    noStroke();
+    fill(this.color);
+    push();
+    translate(this.pos.x, this.pos.y);
+    //파티클 회전각도로 회전
+    rotate(this.rotationA);
+    rectMode(CENTER);
+    rect(0, 0, this.rad, this.rad);
+    pop();
+  }
+
+  isDead() {
+    return this.lifeSpan < 0;
+  }
+}
---
+++ src/w6/assignment/step1/script/sketch.js
@@ -0,0 +1,29 @@
+let particleArray = [];
+let gravity;
+
+function setup() {
+  setCanvasContainer('canvas', 2, 1, true);
+
+  gravity = createVector(0, 0.1);
+
+  background('lightsteelblue');
+}
+
+function draw() {
+  particleArray.push(new Particle(random(width), -10));
+
+  background('lightsteelblue');
+  for (let a = 0; a < particleArray.length; a++) {
+    particleArray[a].applyForce(gravity);
+    particleArray[a].update();
+    particleArray[a].display();
+  }
+
+  for (let a = particleArray.length - 1; a >= 0; a--) {
+    if (particleArray[a].isDead()) {
+      particleArray.splice(a, 1);
+    }
+  }
+
+  console.log(particleArray.length);
+}
